7ee04688128cc780a2f4d9d913773154a4ff70cc
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-19 21:16:39 +0530

    Routing in Next

--- WebDev/nextjscc/pages/about.js
+++ WebDev/nextjscc/pages/about.js
@@ -1,5 +1,11 @@
 const about = () => {
-    return ( <>About!</> );
+    return ( 
+    <div>
+        <h1>About!</h1>
+        <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
+        <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
+    </div> )
+    ;
 }
  
 export default about;
--- WebDev/nextjscc/pages/index.js
+++ WebDev/nextjscc/pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div >
       <h1>Hello!</h1>
+      <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
+      <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
     </div>
   )
 }
