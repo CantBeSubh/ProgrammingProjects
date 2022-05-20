9e5fc4095eb44abe18c881dc683f260a90c250f7
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-20 20:15:44 +0530

    Print path would break something sad

--- WebDev/nextjscc/pages/404.js
+++ WebDev/nextjscc/pages/404.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import { useEffect } from "react";
 
 const NotFound = () => {
-    const { asPath } = useRouter();
+    const router = useRouter();
+    setTimeout(()=>router.push('/'),3000)
+
     return ( 
         <div  className="not-found">
             <h1>Error!</h1>
-            <h2>Page <a>{asPath}</a> found.</h2>
-            <p>Go to <Link href='/'><a>Home</a></Link></p>
+            {/* <h2>Page <a>{router.asPath}</a> found.</h2> */}
+            <p>Going to <Link href='/'><a>Home</a></Link></p>
         </div>
      );
 }
