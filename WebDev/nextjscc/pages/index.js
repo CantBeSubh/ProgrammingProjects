58f84fd9d40e73be4421254a6f8eda46ebb94b24
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-20 17:59:03 +0530

    .

--- WebDev/nextjscc/pages/index.js
+++ WebDev/nextjscc/pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
 
 export default function Home() {
   return (
     <div >
+      <Navbar />
       <h1>Hello!</h1>
       <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
       <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
+      <Footer />
     </div>
   )
 }
---
+++ WebDev/nextjscc/pages/ninjas/index.js
@@ -0,0 +1,14 @@
+const Ninjas = () => {
+    return ( 
+        <>
+            <h1>Ninja Listing</h1>
+            <ul>
+                <li>hello</li>
+                <li>fdsf</li>
+                <li>sdfsdf</li>
+            </ul>
+        </>
+     );
+}
+ 
+export default Ninjas;
