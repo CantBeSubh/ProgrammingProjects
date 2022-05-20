4aeaee187d17fb4c0aa994b65d58cc9bd5f4b4f5
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-20 19:33:08 +0530

    Display webpage name

--- WebDev/nextjscc/pages/404.js
+++ WebDev/nextjscc/pages/404.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+    const { asPath, pathname } = useRouter();
+    console.log(asPath);
     return ( 
         <div  className="not-found">
             <h1>Error!</h1>
-            <h2>Page not found.</h2>
+            <h2>Page {asPath} found.</h2>
             <p>Go to <Link href='/'><a>Home</a></Link></p>
         </div>
      );
