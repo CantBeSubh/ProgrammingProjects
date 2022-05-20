8879351c5641639bbf54b647f12e7548b95ec6b2
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-20 18:35:53 +0530

    Layout that would wrap Final Render

--- WebDev/nextjscc/pages/index.js
+++ WebDev/nextjscc/pages/index.js
@@ -3,15 +3,18 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div >
-      <Navbar />
       <h1>Hello!</h1>
       <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
       <p>Magna commodo nostrud ad amet nulla culpa esse tempor qui aute. Cillum est do eu adipisicing minim commodo cupidatat occaecat ipsum exercitation et ea amet irure. Aliquip duis qui velit esse voluptate nulla reprehenderit excepteur eiusmod.</p>
-      <Footer />
+      <Link href='/ninjas'>
+        <a>See Ninjas list</a>
+      </Link>
+
     </div>
   )
 }
