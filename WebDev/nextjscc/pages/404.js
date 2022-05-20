f635c04899146b685131109969461b64512b9cc7
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-20 19:35:01 +0530

    fixing memory leak

--- WebDev/nextjscc/pages/404.js
+++ WebDev/nextjscc/pages/404.js
@@ -2,12 +2,11 @@ import Link from "next/link";
 import { useRouter } from 'next/router';
 
 const NotFound = () => {
-    const { asPath, pathname } = useRouter();
-    console.log(asPath);
+    const { asPath } = useRouter();
     return ( 
         <div  className="not-found">
             <h1>Error!</h1>
-            <h2>Page {asPath} found.</h2>
+            <h2>Page <a>{asPath}</a> found.</h2>
             <p>Go to <Link href='/'><a>Home</a></Link></p>
         </div>
      );
