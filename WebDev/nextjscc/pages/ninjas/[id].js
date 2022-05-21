c1d36029bae73b73183849de333d602f11231347
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-21 08:54:26 +0530

    Final

--- WebDev/nextjscc/pages/ninjas/[id].js
+++ WebDev/nextjscc/pages/ninjas/[id].js
@@ -1,26 +1,39 @@
-export const getStaticPaths=async ()=>{
-    const res =await fetch('https://jsonplaceholder.typicode.com/users')
-    const data= await res.json()
-    const paths= data.map(elm=>{params:{id:elm.id.toString()}})
-    return{paths,fallback:false}
-}
-export const getStaticProps=async (context)=>{
-    const id=context.params.id
-    const res=await fetch('https://jsonplaceholder.typicode.com/users'+id)
-    const data= await res.json()
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.map(ninja => {
+      return {
+        params: { id: ninja.id.toString() }
+      }
+    })
+  
     return {
-        params:{ninja:data}
+      paths,
+      fallback: false
     }
-}
-const Details = ({ninja}) => {
-    return ( 
-        <div>
-            <h1>{ninja.name}</h1>
-            <h2>{ninja.email}</h2>
-            <h2>{ninja.website}</h2>
-            <h2>{ninja.address.city}</h2>
-        </div>
-     );
-}
- 
-export default Details;
+  }
+  
+  export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+  
+    return {
+      props: { ninja: data }
+    }
+  }
+  
+  const Details = ({ ninja }) => {
+    return (
+      <div>
+        <h1>{ ninja.name }</h1>
+        <p>{ ninja.email }</p>
+        <p>{ ninja.website }</p>
+        <p>{ ninja.address.city }</p>
+      </div>
+    );
+  }
+  
+  export default Details;
--- WebDev/nextjscc/pages/ninjas/index.js
+++ WebDev/nextjscc/pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import ninja from '../../styles/Ninjas.module.css'
 export const getStaticProps=async ()=>{
     const res=await fetch('https://jsonplaceholder.typicode.com/users')
@@ -15,11 +16,11 @@ const Ninjas = (props) => {
         <>
             <h1>Ninja Listing</h1>
             {ninjas.map(({name,id})=>(
-                <div key={id}>
+                <Link href={'/ninjas/'+id} key={id}>
                     <a className={ninja.single}>
                         <h3>{name}</h3>
                     </a>
-                </div>
+                </Link >
             ))}
         </>
      );
