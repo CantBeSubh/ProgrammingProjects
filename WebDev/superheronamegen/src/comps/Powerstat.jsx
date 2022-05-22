91489b41140a635612fa99deb940da62ff1ba005
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-22 20:06:52 +0530

    siuuu graph works

--- WebDev/superheronamegen/src/comps/Powerstat.jsx
+++ WebDev/superheronamegen/src/comps/Powerstat.jsx
@@ -1,42 +1,58 @@
 import React from 'react'
 import {Bar} from '@samsquatch/react-chartjs-2'
 function Powerstat(props) {
-    console.log(props)
+    const {powerstats}=props
+    console.log(powerstats)
+    let labels=[]
+    let data=[]
+    if(powerstats){
+        
+        Object.entries(powerstats).forEach(([key, value]) => {
+            labels.push(key)
+            data.push(value)
+         })
+    }
+    // for(let [i,j] in ){
+    //     console.log(i)
+    // }
   return (
     <div>
         Powerstat
-        <Bar
-        data={ {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets:[{
-                label: '',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'],
-                borderWidth: 1
-            }]
-        }}
-        height={400}
-        width={600}
-        options={
-            {
-                indexAxis: 'y'
-            }
-            }
-        />
+        {powerstats &&
+                <Bar
+                data={ {
+                    labels,
+                    datasets:[{
+                        label: '',
+                        data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'],
+                        borderWidth: 1
+                    }]
+                }}
+                height={400}
+                width={600}
+                options={
+                    {
+                        indexAxis: 'y'
+                    }
+                    }
+                />
+        }
+
     </div>
 
   )
