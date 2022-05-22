2e6cd1f6feee3ae951e462efa657147f24fad794
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-22 13:12:33 +0530

    .

--- WebDev/superheronamegen/src/App.jsx
+++ WebDev/superheronamegen/src/App.jsx
@@ -1,12 +1,12 @@
 import logo from './logo.svg'
-import Name from './Name.jsx'
+import Hero from './Hero.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Super Hero Name Genrator</h1>
-      <Name/>
+      <Hero/>
     </div>
   )
 }
