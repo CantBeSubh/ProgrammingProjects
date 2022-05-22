29c8e334e9eb0af46225dccf18823f8e5f701c10
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-22 21:06:26 +0530

    SIUUUUU HORIZONTAL GRAPh

--- WebDev/superheronamegen/src/comps/Powerstat.jsx
+++ WebDev/superheronamegen/src/comps/Powerstat.jsx
@@ -1,56 +1,67 @@
 import React from 'react'
-import {Bar} from '@samsquatch/react-chartjs-2'
+import {HorizontalBar} from '@samsquatch/react-chartjs-2'
 function Powerstat(props) {
     const {powerstats}=props
-    console.log(powerstats)
+
     let labels=[]
     let data=[]
+    let options={
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                gridLines: {
+                    display: true
+                }
+            }],
+            yAxes: [{
+                gridLines: {
+                display: false
+                }
+            }]
+            
+        },
+        maintainAspectRatio:false
+    }
     if(powerstats){
-        
         Object.entries(powerstats).forEach(([key, value]) => {
-            labels.push(key)
+            labels.push(key.toUpperCase())
             data.push(value)
          })
     }
-    // for(let [i,j] in ){
-    //     console.log(i)
-    // }
+
+    let datasets=[{
+        label: 'Out of 100',
+        data,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'],
+        borderWidth: 1,
+    }]
+
   return (
     <div>
-        Powerstat
+        <h1>Powerstat</h1>
         {powerstats &&
-                <Bar
-                data={ {
-                    labels,
-                    datasets:[{
-                        label: '',
-                        data,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'],
-                        borderWidth: 1
-                    }]
-                }}
-                height={400}
-                width={600}
-                options={
-                    {
-                        indexAxis: 'y'
-                    }
-                    }
-                />
+            <HorizontalBar
+            data={{labels,datasets}}
+            height={200}
+            width={100}
+            options={options}
+            />
         }
 
     </div>
