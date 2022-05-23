4714314793d718ba07d9dc4ea81ae1e325ed8572
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-23 19:44:07 +0530

    base setup

---
+++ WebDev/pwdgen/main.js
@@ -0,0 +1,13 @@
+var hash = CryptoJS.MD5("Message");
+// alert(hash);
+
+function myFunction() {
+  let x = document.getElementById("numb").value;
+  let text;
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = `Input OK | ${CryptoJS.MD5(x)}`;
+  }
+  document.getElementById("demo").innerHTML = text;
+}
