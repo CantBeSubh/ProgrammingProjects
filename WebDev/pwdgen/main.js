fdf8683d4a7a7d562c04c466e163ee912e944b8a
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-23 20:03:18 +0530

    siuuu pwd gen md5

--- WebDev/pwdgen/main.js
+++ WebDev/pwdgen/main.js
@@ -1,13 +1,7 @@
-var hash = CryptoJS.MD5("Message");
-// alert(hash);
-
 function myFunction() {
-  let x = document.getElementById("numb").value;
-  let text;
-  if (isNaN(x) || x < 1 || x > 10) {
-    text = "Input not valid";
-  } else {
-    text = `Input OK | ${CryptoJS.MD5(x)}`;
-  }
-  document.getElementById("demo").innerHTML = text;
+  let name = document.getElementById("name").value;
+  let number = document.getElementById("numb").value;
+  let dob = document.getElementById("dob").value;
+  let text=CryptoJS.MD5(name+dob+number);
+  document.getElementById("demo").innerHTML = `YOUR PWD IS ${text}`;
 }
