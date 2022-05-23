8b28c43cd792e9f97961363845dba834355ac3a6
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-23 15:55:33 +0530

    BOIGRAPHY DONE SIUUU

--- WebDev/superheronamegen/src/comps/Biography.jsx
+++ WebDev/superheronamegen/src/comps/Biography.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-function Biography() {
+function Biography(props) {
+  let bio=props.biography
   return (
-    <div>Biography</div>
+    <div>
+      Biography
+      {bio && 
+      <div>
+        <p>Your name is {bio.fullName}.</p>
+        <p>Your birth place is {bio.placeOfBirth=='-'?'unknown': bio.placeOfBirth}.</p>You first appeared in {bio.firstAppearance}.
+      </div>
+      }
+    </div>
   )
 }
 
