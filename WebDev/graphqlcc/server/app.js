aab1d20e380fa2f7597c8e40f813af6f8b3d793f
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-27 21:10:56 +0530

    siuu

--- WebDev/graphqlcc/server/app.js
+++ WebDev/graphqlcc/server/app.js
@@ -5,5 +5,5 @@ const schema=require('./scheme/schema')
 const app=express()
 const port=3000
 
-app.use('/graphql',graphqlHTTP({schema}))
+app.use('/graphql',graphqlHTTP({schema,graphiql:true}))
 app.listen(port,()=>console.log(`[+] Server stated at port: ${port}`))
