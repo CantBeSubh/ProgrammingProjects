b69689116a7a9f71bffa8ed2fc5698b97d18c050
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-05-29 19:16:41 +0530

    add book form

--- WebDev/graphqlcc/client/src/App.js
+++ WebDev/graphqlcc/client/src/App.js
@@ -1,6 +1,7 @@
 import Booklist from "./comps/BookList";
 
 import {ApolloClient,ApolloProvider,InMemoryCache} from "@apollo/client";
+import AddBook from "./comps/AddBook";
 
 const client=new ApolloClient({
   uri:'http://localhost:3001/graphql',
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <h1>My Reading List</h1>
         <Booklist/>
+        <AddBook/>
       </div>
     </ApolloProvider>
   );
---
+++ WebDev/graphqlcc/client/src/comps/AddBook.js
@@ -0,0 +1,46 @@
+import { gql, useQuery } from "@apollo/client";
+
+const getAuthorsQuery = gql`
+  {
+    authors{
+      name
+      id
+    }
+  }
+`;
+
+function AddBook() {
+  const { loading, error, data } = useQuery(getAuthorsQuery)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  const {authors}=data
+  console.log(authors)
+
+  const displayAuthors=()=>{
+    return authors.map(author=><option key={ author.id } value={author.id}>{ author.name }</option>)
+  }
+  
+  return (
+    <form id="add-book">
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Author:</label>
+        <select>
+          <option>Select author</option>
+          {displayAuthors()}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+}
+
+export default AddBook;
