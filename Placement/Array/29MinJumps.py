5ab926fac922f7584dda52474da41154f6881b34
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-06-18 07:55:04 +0530

    e

---
+++ Placement/Array/29MinJumps.py
@@ -0,0 +1,25 @@
+
+def minJumps(arr):
+    if len(arr) <= 1 : 
+        return 0 
+    if arr[0] == 0 :  
+        return -1 
+    maxReach = arr[0]; 
+    step = arr[0]; 
+    jump = 1; 
+    for i in range(1,len(arr)):
+        if  i == len(arr) - 1 : 
+            return jump 
+        maxReach = max(maxReach, i+arr[i])
+        step-=1; 
+        if  step == 0 : 
+            jump+=1 
+            if i>=maxReach : 
+                return -1
+            step = maxReach - i 
+
+    return -1 
+
+Arr=[1,2,3,4,5]
+ans = minJumps(Arr)
+print(ans)
