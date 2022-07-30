5ab7dc6cb03ce4bbaa660b3dc26709c6eae24e6c
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-07-30 13:19:47 +0530

    gui read input :pogfish:

---
+++ Java/javacc/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class GUI {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter Name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Name"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " y/o");
+
+    }
+}
--- Java/javacc/Main.java
+++ Java/javacc/Main.java
@@ -1,6 +1,11 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Your mom hates you");
+        int a = sc.nextInt();
+        System.out.println("You have entered: " + a);
+        sc.close();
     }
 }
