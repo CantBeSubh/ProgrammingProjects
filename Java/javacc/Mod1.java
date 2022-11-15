6f48ac99fb16ee803edee3c797c1ff387ef6349c
Subhranshu Pati <83113185+h0lycow@users.noreply.github.com>
2022-11-15 22:34:25 +0530

    FAT

---
+++ Java/javacc/Mod1.java
@@ -0,0 +1,7 @@
+public class Mod1 {
+    public static void main(String[] args) {
+        String s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque volutpat dolor vel dui euismod, a egestas enim commodo. Fusce hendrerit";
+        String arr[] = s.split(" ");
+        System.out.println(arr.length);
+    }
+}
