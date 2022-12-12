6dbb068a1a1ac5e6639e2d00c23bb8710b14f5b2
Subhranshu Pati <subhranshupati0412@gmail.com>
2022-12-12 17:26:51 +0530

    Button changes color

--- WebDev/vuejscc/src/components/Header.vue
+++ WebDev/vuejscc/src/components/Header.vue
@@ -3,8 +3,8 @@
         <h1>{{ title }}</h1>
         <Button
             @toggle-add-task="$emit('toggle-add-task')"
-            text="Add Task"
-            color="green"
+            :text="showAddTask ? 'Close' : 'Add'"
+            :color="showAddTask ? 'red' : 'green'"
         />
     </header>
 </template>
@@ -14,7 +14,7 @@ import Button from "./Button";
 
 export default {
     name: "Header",
-    props: ["title"],
+    props: ["title", "showAddTask"],
     components: {
         Button,
     },
