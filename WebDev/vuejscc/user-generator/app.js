4a59c206c35f09926ec7cad9e934f5af072d0c20
Subhranshu Pati <subhranshupati0412@gmail.com>
2022-12-12 13:26:55 +0530

    css

--- WebDev/vuejscc/user-generator/app.js
+++ WebDev/vuejscc/user-generator/app.js
@@ -4,7 +4,6 @@ const app = Vue.createApp({
             firstName: 'John',
             lastName: 'Doe',
             email: 'john@gmail.com',
-            gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
