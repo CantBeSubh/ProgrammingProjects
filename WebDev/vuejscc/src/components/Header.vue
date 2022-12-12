918ff3bc191dcb04b1279f064306d6b13e5cc1dd
Subhranshu Pati <subhranshupati0412@gmail.com>
2022-12-12 15:07:22 +0530

    button

--- WebDev/vuejscc/src/components/Header.vue
+++ WebDev/vuejscc/src/components/Header.vue
@@ -1,21 +1,27 @@
 <template>
     <header>
-        <h1>{{title}}</h1>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green" />
     </header>
 </template>
 
 <script>
-    export default {
-        name: 'Header',
-        props:['title']
-    }   
+import Button from "./Button";
+
+export default {
+    name: "Header",
+    props: ["title"],
+    components: {
+        Button,
+    },
+};
 </script>
 
 <style scoped>
-    header{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-bottom: 20px;
-    }
+header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+}
 </style>
