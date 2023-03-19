96ea147a5b8014daafabcbc5069af862d3eef62e
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-03-19 09:34:39 +0530

    w

--- Placement/Striver/12. Greedy Algorithms/12.2 Med_Hard/NMeetings.cpp
+++ Placement/Striver/12. Greedy Algorithms/12.2 Med_Hard/NMeetings.cpp
@@ -2,46 +2,46 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-bool cmp(string a, string b)
+struct Meet
 {
-    a = a.substr(0, a.size() - 1);
-    b = b.substr(0, b.size() - 1);
-    int x = stoi(a);
-    int y = stoi(b);
-    return x < y;
+    int start;
+    int end;
+    int pos;
+};
+
+bool cmp(Meet a, Meet b)
+{
+    if (a.end == b.end)
+        return a.pos < b.pos;
+    return a.end < b.end;
 }
 
 int soln(int start[], int end[], int n)
 {
-
-    vector<string> v;
+    vector<Meet> m(n);
     for (int i = 0; i < n; i++)
     {
-        v.push_back(to_string(start[i]) + "a");
-        v.push_back(to_string(end[i]) + "d");
+        m[i].start = start[i];
+        m[i].end = end[i];
+        m[i].pos = i + 1;
     }
-    sort(v.begin(), v.end(), cmp);
-    int i = 0, j = 1;
-    int res = 0;
-    for (auto i : v)
-        cout << i << " ";
-    while (j < v.size())
+    sort(m.begin(), m.end(), cmp);
+    int ans = 0;
+    int prev = -1;
+    for (int i = 0; i < n; i++)
     {
-        if ((v[i][v[i].size() - 1] == 'a' && v[j][v[i].size() - 1] == 'd') &&
-            (v[i].substr(0, v[i].size() - 1) != v[j].substr(0, v[j].size() - 1)))
+        if (m[i].start > prev)
         {
-            res++;
-            cout << v[i] << " " << v[j] << endl;
+            ans++;
+            prev = m[i].end;
         }
-        i++;
-        j++;
     }
-    return res;
+    return ans;
 }
 int main()
 {
-    int start[] = {48, 43, 61, 54, 99, 84, 3, 3, 59, 30, 45, 72, 24, 87, 21, 48, 54, 88, 8, 67, 41, 64, 87, 54, 5, 62, 87, 33, 74, 92};
-    int end[] = {150, 67, 137, 131, 139, 115, 49, 6, 117, 126, 59, 109, 27, 96, 73, 60, 99, 108, 50, 145, 68, 104, 102, 82, 7, 126, 118, 93, 148, 150};
-    cout << soln(start, end, 30);
+    int start[] = {1, 3, 0, 5, 8, 5};
+    int end[] = {2, 4, 6, 7, 9, 9};
+    cout << soln(start, end, 6);
     return 0;
 }
